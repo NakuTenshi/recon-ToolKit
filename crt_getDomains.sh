1272dc2d92f2faba5589a4ca03fff2f340ef1ba7
naku.tenshi <naku.tenshii@gmail.com>
2025-12-30 14:49:31 +0330

    cleaning the repo

--- crt_getDomains.sh
+++
@@ -1,8 +0,0 @@
-result=$(curl -s "https://crt.sh/?q=$1&output=json" | jq -r '.[] | .name_value, .common_name' | sort -u)
-
-if [ -n "$2" ]; then
-    echo "$result" > "$2"
-else
-    echo "$result"
-fi
-
--- get_certificate.sh
+++
@@ -1,2 +0,0 @@
-openssl s_client -showcerts -servername $1 -connect $1:443 2> /dev/null | openssl x509 -inform pem -noout -text
-exit 0
