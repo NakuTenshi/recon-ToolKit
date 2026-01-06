6f59c268ce50f2c180c64819bc5bac5094e8cae7
naku.tenshi <naku.tenshii@gmail.com>
2026-01-06 21:20:52 +0330

    adding installtion for anew

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ go install github.com/d3mondev/puredns/v2@latest
 go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
-
+go install -v github.com/tomnomnom/anew@latest
 
 
 shell=$(echo $SHELL | cut -d"/" -f4)
